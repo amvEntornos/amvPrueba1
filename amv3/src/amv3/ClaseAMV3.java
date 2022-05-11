ba905d9d7db82d71c9a76d4134aff8d9b0dee953
amv dos <amv.dos@correo.es>
2022-05-11 21:24:33 +0200

    líneas

--- amv3/src/amv3/ClaseAMV3.java
+++ amv3/src/amv3/ClaseAMV3.java
@@ -3,11 +3,13 @@ package amv3;
 public class ClaseAMV3 {
 
 	public static void main(String[] args) {
+		System.out.println("-------------------");
 		System.out.println("amv3");
 		for(int i=1;i<=9;i++) {
 			System.out.print(i);
 		}
 		System.out.println();
+		System.out.println("-------------------");
 		
 	}
 
