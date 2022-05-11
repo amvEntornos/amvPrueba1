6e630bbdec2a72378ec7db738f79ee65f989fa49
AMV Mint <amv.mint@correo.es>
2022-05-11 21:13:01 +0200

    bucle amv3

--- amv3/src/amv3/ClaseAMV3.java
+++ amv3/src/amv3/ClaseAMV3.java
@@ -4,6 +4,11 @@ public class ClaseAMV3 {
 
 	public static void main(String[] args) {
 		System.out.println("amv3");
+		for(int i=1;i<=9;i++) {
+			System.out.print(i);
+		}
+		System.out.println();
+		
 	}
 
 }
